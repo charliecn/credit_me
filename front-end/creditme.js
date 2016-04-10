b07860cb856a1297e4e6a811173ce38b9eaddbb8
lmhly <Lucy_mouse@lucydembp.devices.brown.edu>
2016-04-09 23:38:33 -0400

    Setup front-end home page & user input page
    There are two buttons sell and buy on the home page, both link to the
    input page. In the final version, the dining halls should be dropdown
    menus, but I use datalist to do it for now to keep it simple. The two
    pairs of upper/lower bounds alerts when lower > upper, but this should
    be more user-friendly in the future.

---
+++ front-end/creditme.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+	var sellButton = document.getElementById("sell_button");
+	sellButton.addEventListener('click', function(){
+		window.location = 'userinput.html';
+	});
+});
---
+++ front-end/userinput.js
@@ -0,0 +1,56 @@
+var upperPrice = 0.0;
+var lowerPrice = 0.0;
+var upperTime = 0;
+var lowerTime = 0;
+
+
+window.addEventListener('load', function(){
+	var upperRange = document.getElementById("upper_range");
+	upperRange.addEventListener('change', function(){
+		upperPrice = (this.value/50) + 7;
+		if (lowerPrice === 0.0 || lowerPrice < upperPrice) {
+			document.getElementById("upper_price").innerHTML = upperPrice;
+		} else {
+			alert("upper should be bigger than lower!");
+		}
+	});
+	var lowerRange = document.getElementById("lower_range");
+	lowerRange.addEventListener('change', function(){
+		//alert(this.value);
+		//var val = 0.0;
+		lowerPrice = (this.value/50) + 7;
+		if (upperPrice === 0.0 || upperPrice > lowerPrice) {
+			document.getElementById("lower_price").innerHTML = lowerPrice;
+		} else {
+			alert("upper should be bigger than lower!");
+		}
+		//window.location = 'userinput.html';
+	});
+	var upperTimeRange = document.getElementById("upper_time_range");
+	upperTimeRange.addEventListener('change', function(){
+		//alert(this.value);
+		//var val = 0.0;
+		upperTime = this.value;
+		//alert(upperTime + " " + lowerTime);
+		if (lowerTime === 0 || lowerTime < upperTime) {
+			document.getElementById("upper_time").innerHTML = upperTime;
+		} else {
+			alert("upper should be bigger than lower!");
+		}
+		//window.location = 'userinput.html';
+	});
+	var lowerTimeRange = document.getElementById("lower_time_range");
+	lowerTimeRange.addEventListener('change', function(){
+		//alert(this.value);
+		//var val = 0.0;
+		lowerTime = this.value;
+		//alert(upperTime + " " + lowerTime);
+		if (upperTime === 0 || upperTime > lowerTime) {
+			document.getElementById("lower_time").innerHTML = lowerTime;
+		} else {
+			alert("upper should be bigger than lower!");
+		}
+		//window.location = 'userinput.html';
+	});
+
+});

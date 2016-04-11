44c984774910e1cd1e340d82ee765a1ebd265502
lmhly <Lucy_mouse@lucydembp.devices.brown.edu>
2016-04-10 20:28:07 -0400

    changed slider according to silei's advice
    However this does not look as intuitive as I thought it should be. But
    this reflects the basic idea and should be good enough for tomorrow’s
    checkpoint.

--- front-end/userinput.js
+++ front-end/userinput.js
@@ -5,51 +5,28 @@ var lowerTime = 0;
 
 window.addEventListener('load', function(){
 	var upperRange = document.getElementById("upper_range");
+	var lowerRange = document.getElementById("lower_range");
 	upperRange.addEventListener('change', function(){
 		upperPrice = (this.value/50) + 7;
-		if (lowerPrice === 0.0 || lowerPrice < upperPrice) {
-			document.getElementById("upper_price").innerHTML = upperPrice;
-		} else {
-			alert("upper should be bigger than lower!");
-		}
+		lowerRange.max = this.value;
+		document.getElementById("upper_price").innerHTML = upperPrice;
 	});
-	var lowerRange = document.getElementById("lower_range");
 	lowerRange.addEventListener('change', function(){
-		//alert(this.value);
-		//var val = 0.0;
 		lowerPrice = (this.value/50) + 7;
-		if (upperPrice === 0.0 || upperPrice > lowerPrice) {
-			document.getElementById("lower_price").innerHTML = lowerPrice;
-		} else {
-			alert("upper should be bigger than lower!");
-		}
-		//window.location = 'userinput.html';
+		upperRange.min = this.value;
+		document.getElementById("lower_price").innerHTML = lowerPrice;
 	});
 	var upperTimeRange = document.getElementById("upper_time_range");
-	upperTimeRange.addEventListener('change', function(){
-		//alert(this.value);
-		//var val = 0.0;
-		upperTime = this.value;
-		//alert(upperTime + " " + lowerTime);
-		if (lowerTime === 0 || lowerTime < upperTime) {
-			document.getElementById("upper_time").innerHTML = upperTime;
-		} else {
-			alert("upper should be bigger than lower!");
-		}
-		//window.location = 'userinput.html';
-	});
 	var lowerTimeRange = document.getElementById("lower_time_range");
+	upperTimeRange.addEventListener('change', function(){
+		upperTime = this.value;
+		lowerTimeRange.max = this.value;
+		document.getElementById("upper_time").innerHTML = upperTime;
+	});
 	lowerTimeRange.addEventListener('change', function(){
-		//alert(this.value);
-		//var val = 0.0;
 		lowerTime = this.value;
-		//alert(upperTime + " " + lowerTime);
-		if (upperTime === 0 || upperTime > lowerTime) {
-			document.getElementById("lower_time").innerHTML = lowerTime;
-		} else {
-			alert("upper should be bigger than lower!");
-		}
-		//window.location = 'userinput.html';
+		upperTimeRange.min = this.value;
+		document.getElementById("lower_time").innerHTML = lowerTime;
 	});
 
 	document.getElementById("userinput_field").addEventListener('submit', function(e){

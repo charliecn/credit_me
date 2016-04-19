ee2321002e2a84945fb0f1deca94eebd26395824
Lucie Ackley <lucile_ackley@brown.edu>
2016-04-19 11:44:47 -0400

    finished Query

--- backend/src/main/java/database/Query.java
+++ backend/src/main/java/database/Query.java
@@ -3,6 +3,8 @@ package database;
 import user.User;
 import user.BrownUser;
 import user.GuestUser;
+import locationfood.Eatery;
+import locationfood.Food;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -45,6 +47,37 @@ public class Query {
 			return new GuestUser(name, email, password, subscribe);
 		}
 	}
+	
+	/**
+	 * Get a user just given email address.
+	 * @param email - user's email
+	 * @param conn - connection to database
+	 * @return User if there is a user with this email, null otherwise
+	 * @throws SQLException - if unable to properly connect with db
+	 */
+	public static User getUser(String email, Connection conn) throws SQLException{
+		PreparedStatement prep = 
+				conn.prepareStatement("SELECT name, subscribe FROM user WHERE email = ?");
+		
+		prep.setString(1, email);
+		
+		ResultSet rs = prep.executeQuery();
+		
+		//should only be one user with email
+		if(rs.next() == false){
+			return null;
+		}
+
+		String name = rs.getString("name");
+		Boolean subscribe = rs.getBoolean("subscribe");
+		String password = rs.getString("password");
+		
+		if(isBrownEmail(email)){
+			return new BrownUser(name, email, password, subscribe);
+		} else {
+			return new GuestUser(name, email, password, subscribe);
+		}
+	}
 
 	/**
 	 * Determines whether string is a brown email address.
@@ -59,17 +92,31 @@ public class Query {
 		return email.substring(atInd).equals("brown.edu");
 	}
 	
-	public static boolean deleteUser(User user, Connection conn){
+	/**
+	 * Delete a user from the user table.
+	 * @param email - user's email
+	 * @param password - user's password
+	 * @param conn - connection to the database
+	 * @return true if able to find and delete user, false otherwise
+	 */
+	public static boolean deleteUser(String email, String password, Connection conn){
 		PreparedStatement prep;
 		try {
-			prep = conn.prepareStatement("DELETE FROM user WHERE email = ?");
-			prep.setString(1, user.getEmail());
+			prep = conn.prepareStatement("DELETE FROM user WHERE email = ? AND password = ?");
+			prep.setString(1, email);
+			prep.setString(2, password);
 		} catch (SQLException e) {
 			return false;
 		}
 		return true;
 	}
 	
+	/**
+	 * Adds a user to the user table
+	 * @param user - user to add
+	 * @param conn - connection to database
+	 * @return true if able to successfully add user, false otherwise
+	 */
 	public static boolean putUser(User user, Connection conn){
 		PreparedStatement prep;
 		try {
@@ -85,9 +132,7 @@ public class Query {
 			//TODO: WHAT IS A TITLE STRING
 			String titleString = "?";
 			prep.setString(7, titleString);
-			//TODO: WHAT IS SUBSCRIBE
-			String subscribe = "?";
-			prep.setString(8, subscribe);
+			prep.setBoolean(8, user.getSubscribe());
 		} catch (SQLException e) {
 			return false;
 		}
@@ -108,4 +153,60 @@ public class Query {
 		}
 		return true;
 	}
+	
+	/**
+	 * Get the location with the given name if it exists.
+	 * @param eateryName - name of the eatery
+	 * @param conn - connection to database
+	 * @return the location with given name if it exists, null otherwise
+	 * @throws SQLException - if unable to properly connect to db
+	 */
+	public static Eatery getEatery(String eateryName, Connection conn) throws SQLException{
+		PreparedStatement prep = 
+				conn.prepareStatement("SELECT id, section, start, end, description FROM eatery WHERE name = ?");
+		
+		prep.setString(1, eateryName);
+		
+		ResultSet rs = prep.executeQuery();
+		
+		if(rs.next() == false){
+			return null;
+		}
+		String id = rs.getString("id");
+		String section = rs.getString("section");		
+		int start = rs.getInt("start");
+		int end = rs.getInt("end");
+		String description = rs.getString("description");
+			
+		return new Eatery(id, eateryName, section, start, end, description);
+		
+		
+
+	}
+	
+	/**
+	 * Gets the food with the given foodId.
+	 * @param foodId - id of food
+	 * @param conn - connection to database
+	 * @return the Food with given id if it exists, null otherwise
+	 * @throws SQLException if unable to properly connect
+	 */
+	public static Food getFood(String foodId, Connection conn) throws SQLException{
+		PreparedStatement prep = 
+				conn.prepareStatement("SELECT name, price FROM food WHERE foodId = ?");
+		
+		prep.setString(1, foodId);
+		
+		ResultSet rs = prep.executeQuery();
+		if(rs.next() == false){
+			return null;
+		}
+		
+		String name = rs.getString("name");
+		Double price = rs.getDouble("price");
+		return new Food(price, name);
+		
+	}
+	
+	
 }
--- backend/src/main/java/locationfood/Location.java
+++ backend/src/main/java/locationfood/Location.java
@@ -1,13 +1,47 @@
 package locationfood;
 
+/**
+ * Represents any location 
+ * @author lucieackley
+ *
+ */
 public class Location {
-  public Type type;
 
-  public enum Type {
-    eatery, normal;
-  }
+	private String id;
+	private String name;
+	private String section;
+	private String description;
 
-  public Type getType() {
-    return null;
-  }
+	/**
+	 * A location constructor.
+	 * @param id - id of location
+	 * @param name - name of location 
+	 * @param section - section on map
+	 * @param description - description of location
+	 */
+	public Location(String id, String name, String section, String description){
+		this.id = id;
+		this.name = name;
+		this.section = section;
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getSection() {
+		return section;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+
 }

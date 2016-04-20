7ca002f8b51ae8b95b6ff38379eab5c4d924a86f
Lucie Ackley <lucile_ackley@brown.edu>
2016-04-19 20:12:53 -0400

    added javax.mail jar

--- backend/src/main/java/emailer/EmailSender.java
+++ backend/src/main/java/emailer/EmailSender.java
@@ -1,38 +1,47 @@
 package emailer;
 
 import java.util.*;
-import java.io.*;
-import java.nio.file.*;
+
 import javax.mail.*;
 import javax.mail.internet.*;
 
-import org.eclipse.jetty.http.HttpTester.Message;
-
-
 
 public class EmailSender {
-	public void sendEmail(
-		    String aFromEmailAddr, String aToEmailAddr,
-		    String aSubject, String aBody
-		  ){
-		    //Here, no Authenticator argument is used (it is null).
-		    //Authenticators are used to prompt the user for user
-		    //name and password.
-		    Session session = Session.getDefaultInstance(fMailServerConfig, null);
-		    MimeMessage message = new MimeMessage(session);
-		    try {
-		      //the "from" address may be set in code, or set in the
-		      //config file under "mail.from" ; here, the latter style is used
-		      //message.setFrom(new InternetAddress(aFromEmailAddr));
-		      message.addRecipient(
-		        Message.RecipientType.TO, new InternetAddress(aToEmailAddr)
-		      );
-		      message.setSubject(aSubject);
-		      message.setText(aBody);
-		      Transport.send(message);
-		    }
-		    catch (MessagingException ex){
-		      System.err.println("Cannot send email. " + ex);
-		    }
-		  }
+	public void sendEmail(String to, String from, String subject, String body)
+	{    
+
+		// host right now is set to local houst
+		String host = "localhost";
+
+		// Get system properties
+		Properties properties = System.getProperties();
+
+		// Setup mail server
+		properties.setProperty("mail.smtp.host", host);
+
+		Session session = Session.getDefaultInstance(properties);
+
+		try{
+			
+			MimeMessage message = new MimeMessage(session);
+
+			// set who it is from 
+			message.setFrom(new InternetAddress(from));
+
+			// set who it is to 
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+			// sets the subject
+			message.setSubject(subject);
+
+			//body of the message
+			message.setText(body);
+
+			//sends the actual message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		}catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 }
